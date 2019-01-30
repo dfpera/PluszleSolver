7cc4ffb06e542f69f1c730e6ae479f37877494a4
Daniele Perazzolo <dfperazzolo@gmail.com>
2019-01-29 23:06:41 -0800

    Add auto tabbing after numbers are entered.

--- process/js/script.js
+++ process/js/script.js
@@ -1,14 +1,15 @@
-// var grid = new Grid(
-//   [
-//     2, 3, 3, 5, 6, 6, 3, 5, 4, 8, 3, 6, 5, 5, 4, 8, 5, 1, 9, 3, 6, 5, 5, 9, 8, 5, 9, 4, 4, 6, 6, 2, 8, 3, 6, 2
-//   ],
-//   [19, 3, 22, 11, 27, 22],
-//   [24, 5, 27, 18, 16, 14]
-// );
-// var result = grid.solveGrid();
-// console.log(grid);
-// console.log("==== GRID ====");
-// grid.print(grid.grid);
-// console.log("==== RESULT ====");
-// grid.print(grid.result);
+document.querySelectorAll('input.num').forEach((input) => {
+  input.addEventListener('keyup', function() {
+    input.nextSibling.nextSibling.focus();
+    input.nextSibling.nextSibling.value = '';
+  });
+});
 
+document.querySelectorAll('input.row, input.column').forEach((input) => {
+  input.addEventListener('keyup', function() {
+    if (input.value.length == 2) {
+      input.nextSibling.nextSibling.focus();
+      input.nextSibling.nextSibling.value = '';
+    }
+  });
+});
